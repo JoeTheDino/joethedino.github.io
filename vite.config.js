fbebc36e0a7c339b69ffa4dc408c85c006b74cbd
JoeTheDino <joeangry@gmail.com>
2025-07-17 14:25:54 +0200

    Ajout gh-pages et fix textures

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: '/WebAR-app/',
-   /* 
+
     build: {
+        outDir: 'dist',
         minify: false
     }
-    */
     /* server: {
         host:true,
         allowedHosts: ['localhost','1de4f70d2808.ngrok-free.app'],
